167b58efadda47a25790be1923f2b6c2185bc361
Joao Marcos Silva <joaoteixeirasilva33@gmail.com>
2024-03-22 22:23:18 -0300

    project done for sucess

---
+++ angular-site/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ContactpageComponent } from './pages/contactpage/contactpage.component';
+
+export const routes: Routes = [
+    { path: "", component: HomepageComponent },
+    { path: "contact-us", component: ContactpageComponent }
+];
